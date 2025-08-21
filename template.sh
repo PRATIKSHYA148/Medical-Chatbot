cf239a8363433f73a3162e20c568403676d57f74
Pratikshya Mishra <pratiksha9436@gmail.com>
2025-08-21 17:00:28 +0545

    folder added

---
+++ template.sh
@@ -0,0 +1,15 @@
+#creating directories
+mkdir -p src
+mkdir -p research
+
+#creating file
+touch src/_init_.py_
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.py
+touch requirements.txt
+
+echo "Directory and files created successfully."
